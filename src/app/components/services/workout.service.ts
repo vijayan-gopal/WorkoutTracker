881287ac0ff7ade744448ab25a93f55537adb938
Vijayan Gopal <vijayan.gopal2@gmail.com>
2018-09-18 21:08:06 -0400

    changed url for add/delete category

--- src/app/components/services/workout.service.ts
+++ src/app/components/services/workout.service.ts
@@ -9,13 +9,13 @@ export class WorkoutService {
   constructor(private http: Http) { }
 
   public addWorkout(category: String): Promise<any> {
-    return this.http.post('http://localhost:3000/add',{"category_name":category}).toPromise() 
+    return this.http.post('http://localhost:3000/addcategory',{"category_name":category}).toPromise() 
   };
 
   public getWorkout(): Promise<any> {
-    return this.http.get('http://localhost:3000/').toPromise()
+    return this.http.get('http://localhost:3000/fetchcategory').toPromise()
   };
   public removeWorkout(category: String): Promise<any> {
-  return this.http.post('http://localhost:3000/delete',{"category_name":category}).toPromise()
+  return this.http.post('http://localhost:3000/deletecategory',{"category_name":category}).toPromise()
   };
 }
